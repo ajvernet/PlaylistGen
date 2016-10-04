e6a764a5ac9c0e54b12a7f213b64b2cbefd2e6c6
thurston ssa <thurston.nabe@theironyard.com>
2016-10-04 13:33:11 -0400

    The basics of a proper address->state->zip, just fill in the state enum

--- src/test/java/org/ssa/ironyard/model_tests/AddressTest.java
+++ src/test/java/org/ssa/ironyard/model_tests/AddressTest.java
@@ -3,12 +3,15 @@ package org.ssa.ironyard.model_tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.ssa.ironyard.model.Address;
+import org.ssa.ironyard.model.Address.ZipCode;
 
 public class AddressTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void zippy() {
+        ZipCode zipCode = new ZipCode("89078-0987");
+        assertEquals("", "890780987", zipCode.datafy());
     }
 
 }
