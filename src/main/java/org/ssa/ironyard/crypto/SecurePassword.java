153b7a81bf3442b7696fc0491ba784cb3ff3a23c
Alexander Vernet <alexanderjvernet@gmail.com>
2016-10-05 13:00:08 -0400

    1:00 pm 10/5

---
+++ src/main/java/org/ssa/ironyard/crypto/SecurePassword.java
@@ -0,0 +1,26 @@
+package org.ssa.ironyard.crypto;
+
+import org.ssa.ironyard.model.Password;
+
+/**
+ *
+ * @author thurston
+ */
+public interface SecurePassword 
+{
+
+    /**
+     *
+     * @param clearText a raw password, un-hashed, un-encrypted
+     * @return a Password suitable for secure persistent storage
+     */
+    Password secureHash(String clearText);
+    
+    /**
+     * Does the <code>clearText</code> match the secure password?
+     * @param clearText
+     * @param secure
+     * @return
+     */
+    boolean verify(String clearText, Password secure);
+}
