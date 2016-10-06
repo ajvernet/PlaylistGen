3fcdcdd3c1f811ca6942205a6094537f79211c80
wormholeracing <andyjm9@gmail.com>
2016-10-06 12:57:43 -0400

    Added view to LoginController

--- src/main/java/org/ssa/ironyard/controller/LoginController.java
+++ src/main/java/org/ssa/ironyard/controller/LoginController.java
@@ -6,10 +6,13 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.InternalResourceView;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.InternalResourceView;
 import org.ssa.ironyard.model.ResponseObject;
 import org.ssa.ironyard.model.ResponseObject.STATUS;
 import org.ssa.ironyard.model.User;
@@ -34,13 +37,17 @@ public class LoginController {
 	String password = request.getParameter("pass");
 	LOGGER.debug("Processing login attempt for {} - {}", username, password);
 	User user = loginService.authenticateUser(username, password);
-	session.setAttribute("UserID", user.getId());
 	if (user == null)
 	    return ResponseEntity.ok()
 		    .body(ResponseObject.instanceOf(STATUS.ERROR, "Username or password invalid", null));
-
+	session.setAttribute("UserID", user.getId());
 	return ResponseEntity.ok()
 		.body(ResponseObject.instanceOf(STATUS.SUCCESS, "Congratulations, you are a valid user", user.getId()));
 
+    }
+    
+    @RequestMapping(value="login", method=RequestMethod.GET)
+    public View showLoginPage(){
+	return new InternalResourceView("/login.html");
     }
 }
