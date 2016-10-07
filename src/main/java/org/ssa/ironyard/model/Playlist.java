9e5f710d13ad4fbd70039f6daf6922e2c513f030
wormholeracing <andyjm9@gmail.com>
2016-10-06 20:07:38 -0400

    updated domain objects

--- src/main/java/org/ssa/ironyard/model/Playlist.java
+++ src/main/java/org/ssa/ironyard/model/Playlist.java
@@ -1,94 +1,68 @@
 package org.ssa.ironyard.model;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Playlist extends DomainObject {
 
-public class Playlist extends DomainObject{
+    private final String name;
+    private final User user;
 
-    final User user;
-    final List<Episode> podcasts = new ArrayList<>();
-    
-    
-    public Playlist(Integer id, boolean loaded, User user) {
-        super(id, loaded);
-        this.user = user;
-        // TODO Auto-generated constructor stub
+    public Playlist(Integer id, boolean loaded, String name, User user) {
+	super(id, loaded);
+	this.name = name;
+	this.user = user;
     }
-    
-    public Playlist()
-    {
-        this(null, false, null);
+
+    public String getName() {
+	return name;
     }
-    
-    public List<Episode> getPodcasts()
-    {
-        return new ArrayList<>(this.podcasts);
-    }
-    
-    public List<Episode> addPodcast(Episode podcast)
-    {
-        this.podcasts.add(podcast);
-        return new ArrayList<>(this.podcasts);
-    }
-    
-    public List<Episode> deletePodcast(int podcastId)
-    {
-        this.podcasts.remove(podcastId);
-        return new ArrayList<>(this.podcasts);
-    }
-    
-    public List<Episode> addPodcasts(List<Episode> podcasts)
-    {
-        this.podcasts.addAll(podcasts);
-        return new ArrayList<>(this.podcasts);
-    }
-    
-    public List<Episode> replacePodcasts(List<Episode> podcasts)
-    {
-        this.podcasts.clear();
-        this.podcasts.addAll(podcasts);
-        
-        return new ArrayList<>(this.podcasts);
-        
-    }
-    
-    public Playlist setId(Integer id)
-    {
-        return new Playlist(id, this.loaded, this.user);
-    }
-    
-    public Playlist setLoaded()
-    {
-        return new Playlist(this.id, true, this.user);
-    }
-    
-    public Playlist setUser(User user)
-    {
-        return new Playlist(this.id, this.loaded, user);
-    }
-    @Override
-    public boolean deeplyEquals(Object obj) {
-        if (this == obj)
-            return true;
-        if (!super.equals(obj))
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Playlist other = (Playlist) obj;
-        if (podcasts == null) {
-            if (other.podcasts != null)
-                return false;
-        } else if (!podcasts.equals(other.podcasts))
-            return false;
-        return true;
+
+    public User getUser() {
+	return user;
     }
 
     @Override
-    public String toString() {
-        return super.toString() + "PlayList [podcasts=" + podcasts + "]";
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	DomainObject other = (DomainObject) obj;
+	if (id == null) {
+	    return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	return true;
+    }
 
-    
-    
+    @Override
+    boolean deeplyEquals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Playlist other = (Playlist) obj;
+	if (name == null) {
+	    if (other.name != null)
+		return false;
+	} else if (!name.equals(other.name))
+	    return false;
+	if (user == null) {
+	    if (other.user != null)
+		return false;
+	} else if (!user.equals(other.user))
+	    return false;
+	return true;
+    }
+
 }
--- src/main/java/org/ssa/ironyard/model/Show.java
+++ src/main/java/org/ssa/ironyard/model/Show.java
@@ -1,52 +1,68 @@
 package org.ssa.ironyard.model;
 
-    public class Show extends DomainObject{
+public class Show extends DomainObject {
 
-        final String name;
-        
+    final String name;
+    final Integer showId;
 
-        public Show(Integer id, boolean loaded, String name) {
-            super(id, loaded);
-            this.name = name;
-        }
-        
-        public Show(String name)
-        {
-            this(null, false, name);
-        }
+    public Show(Integer id, boolean loaded, String name, Integer showId) {
+	super(id, loaded);
+	this.name = name;
+	this.showId = showId;
+    }
 
-        public Show setId(Integer id)
-        {
-            return new Show(id, this.loaded, this.name);
-        }
-        
-        public Show setLoaded()
-        {
-            return new Show(this.id, true, this.name);
-        }
-        
-        @Override
-        public boolean deeplyEquals(Object obj) {
-            if (this == obj)
-                return true;
-            if (!super.equals(obj))
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Show other = (Show) obj;
-            if (name == null) {
-                if (other.name != null)
-                    return false;
-            } else if (!name.equals(other.name))
-                return false;
-            return true;
-        }
+    public String getName() {
+	return name;
+    }
 
-        @Override
-        public String toString() {
-            return super.toString() + "PodcastSeries [name=" + name + "]";
-        }
-        
-        
-    
+    public Integer getShowId() {
+	return showId;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	DomainObject other = (DomainObject) obj;
+	if (id == null) {
+	    return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	return true;
+    }
+
+    @Override
+    boolean deeplyEquals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Show other = (Show) obj;
+	if (name == null) {
+	    if (other.name != null)
+		return false;
+	} else if (!name.equals(other.name))
+	    return false;
+	if (showId == null) {
+	    if (other.showId != null)
+		return false;
+	} else if (!showId.equals(other.showId))
+	    return false;
+	return true;
+    }
+
 }
