92c979633cf7b4e34fa8267e9d4db62ede8db48b
Alexander Vernet <alexanderjvernet@gmail.com>
2016-10-11 16:00:44 -0400

    pulling playlistdao interface

--- src/test/java/org/ssa/ironyard/service/PlaylistServiceTest.java
+++ src/test/java/org/ssa/ironyard/service/PlaylistServiceTest.java
@@ -2,6 +2,7 @@ package org.ssa.ironyard.service;
 
 import static org.junit.Assert.*;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 import org.ssa.ironyard.crypto.BCryptSecurePassword;
@@ -19,47 +20,15 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 public class PlaylistServiceTest {
 
     PlaylistService testService;
-    PlaylistDAO playlistDAO;
-    EpisodeDAO episodeDAO;
-    MysqlDataSource dataSource;
-    static String URL = "jdbc:mysql://localhost/Playlistdb?" + "user=root&password=root&" + "useServerPrepStmts=true";
-  
-    User user;
-    User user2;
     
-    Playlist list1;
-    Playlist list2;
-    
-    Episode episode1;
-    Episode episode2;
+
+ 
     
     @Before
-    public void setUp()
+    public void mock()
     {
-        dataSource = new MysqlDataSource();
-        dataSource.setURL(URL);
+        this.testService = EasyMock.createNiceMock(PlaylistService.class);
         
-        playlistDAO = new PlaylistDAO(dataSource);
-        episodeDAO = new EpisodeDAO(dataSource);
-        
-        testService = new PlaylistService(playlistDAO, episodeDAO, dataSource);
-  
-        playlistDAO.clear();
-        episodeDAO.clear();
-        
-        user = User.builder().email("test@test.com").firstName("Bob").lastName("Loblaw")
-                .address(Address.builder().street("123 Mockingbird Ln").city("Mockingbird Heights").state(State.ALABAMA)
-                    .zip(new ZipCode("12345")).build())
-                .password(new BCryptSecurePassword().secureHash("munsters")).build();
-
-        user2 = User.builder(user).email("Test2@test.com").build();
-        
-        episode1 = Episode.builder().episodeId(1).duration(1000).fileUrl("http://test.com/test").name("TestCast Audio").build();
-        episode2 = Episode.builder().episodeId(2).duration(100).fileUrl("http://test.com/test2").name("TestCast Audio2").build();
-        
-        list1 = Playlist.builder().name("testPlaylist").user(user).targetDuration(1000).addEpisode(episode1).addEpisode(episode2).build();
-        list2 = Playlist.builder().name("testPlaylist2").user(user).targetDuration(1000).addEpisode(episode1).build();
-
     }
     
     @Test
