de27e18567e0d350313c462b2dd332c0334d8df8
Alexander Vernet <alexanderjvernet@gmail.com>
2016-10-12 09:06:29 -0400

    pulling

--- src/main/java/org/ssa/ironyard/service/PlaylistService.java
+++ src/main/java/org/ssa/ironyard/service/PlaylistService.java
@@ -1,28 +1,65 @@
 package org.ssa.ironyard.service;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.ssa.ironyard.dao.EpisodeDAO;
+import org.ssa.ironyard.dao.PlaylistDAO;
 import org.ssa.ironyard.dao.PlaylistDAOImpl;
+import org.ssa.ironyard.model.Episode;
 import org.ssa.ironyard.model.Playlist;
 
 public class PlaylistService {
     
-    private final PlaylistDAOImpl playlistDao;
+    
+    final PlaylistDAO playlistDao;
+    final EpisodeDAO episodeDao;
     
     @Autowired
     public PlaylistService(DataSource dataSource){
-	playlistDao = new PlaylistDAOImpl(dataSource);
+	this.playlistDao = new PlaylistDAOImpl(dataSource);
+	this.episodeDao = new EpisodeDAO(dataSource);
     }
     
+    public PlaylistService(PlaylistDAO playlistDao, EpisodeDAO episodeDao,
+    DataSource dataSource){
+        this.playlistDao = new PlaylistDAOImpl(dataSource);
+        this.episodeDao = new EpisodeDAO(dataSource);
+       
+    }
+
+    
+    
     public Playlist getPlaylistById(int id){
-	return playlistDao.read(id);
+        return playlistDao.read(id);
+    }
+    
+    public List<Playlist> getPlaylistsByUser(int userId)
+    {
+        return playlistDao.readByUser(userId);
+    }
+    
+    public boolean deletePlaylist(int id)
+    {
+        return playlistDao.delete(id);
     }
     
     public Playlist savePlaylist(Playlist playlist){
-	if(playlist.getId() == null)
+	if(playlist.getId() == null){
+	    for(Episode e: playlist.getEpisodes())
+	        episodeDao.insert(e);
+	    playlistDao.replaceEpisodes(playlist.getId(), playlist.getEpisodes());
 	    return playlistDao.insert(playlist);
-	else return playlistDao.update(playlist);
+	}
+	else {
+	       for(Episode e: playlist.getEpisodes())
+	            episodeDao.insert(e);
+	    playlistDao.replaceEpisodes(playlist.getId(), playlist.getEpisodes());
+	       
+	    return playlistDao.update(playlist);
+	    }
     }
     
     
