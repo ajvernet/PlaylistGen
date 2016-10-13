da033a94cf196488516c61e33fc1c8a0329d5cb9
Alexander Vernet <alexanderjvernet@gmail.com>
2016-10-13 12:44:01 -0400

    pulling 10/13

--- src/test/java/org/ssa/ironyard/controllers/PlaylistControllerTest.java
+++ src/test/java/org/ssa/ironyard/controllers/PlaylistControllerTest.java
@@ -2,11 +2,9 @@ package org.ssa.ironyard.controllers;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.easymock.EasyMock;
@@ -21,14 +19,11 @@ import org.ssa.ironyard.model.Playlist;
 import org.ssa.ironyard.model.User;
 import org.ssa.ironyard.service.PlaylistService;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-
 public class PlaylistControllerTest {
 
     PlaylistController controller;
     PlaylistService service;
-    Map<String, String> map = new HashMap<>();
+    Map<String, Object> map = new HashMap<>();
     Playlist list1;
     
     
@@ -37,35 +32,28 @@ public class PlaylistControllerTest {
     {
         service = EasyMock.createNiceMock(PlaylistService.class);
         controller = new PlaylistController(service);
-        
-        map.put("name", "Alex");
-        map.put("user", User.builder().id(1).build().toString());
-        map.put("targetDuration", "500");
-        map.put("episodeDuration", "450");
-        map.put("episodes", new ArrayList<>(Arrays.asList(Episode.builder().episodeId(2))).toString());
+        map.add("name", "Alex");
+        map.add("user", User.builder().id(1).build());
+        map.add("targetDuration", 500);
+        map.add("episodeDuration", 450);
+        map.add("episodes", new ArrayList<>(Arrays.asList(Episode.builder().episodeId(2))));
        
-        List<Episode> episodes = new ArrayList<>();
-        episodes.add(Episode.builder().id(5).build());
-        
-        
         list1 = Playlist.builder().name("Alex")
                 .user(User.builder().id(1).build())
                 .targetDuration(500)
                 .currentDuration(450)
-                .episodes(episodes)
+                .episodes(new ArrayList<>(Arrays.asList((Episode.builder().episodeId(2).build()))))
                 .build();
     }
     
 
     @Test
-    public void savePlaylistTest() throws JsonParseException, JsonMappingException, IOException 
+    public void savePlaylistTest() 
     {
        EasyMock.expect(this.service.savePlaylist(list1)).andReturn(list1);
        EasyMock.replay(this.service);
-       
-       //this.controller.createPlaylist(this.map, 1);
-     //  System.out.println(this.controller.createPlaylist(this.map).getBody());
-      // assertTrue(this.controller.createPlaylist(this.map).getBody().deeplyEquals(list1));
+       System.out.println(this.controller.createPlaylist(this.map, 1));
+       assertTrue(this.controller.createPlaylist(this.map).getBody().deeplyEquals(list1));
        
        EasyMock.verify(this.service);
        
