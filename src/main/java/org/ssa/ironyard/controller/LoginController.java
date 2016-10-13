d101a5dfbfd78ea38e3ed4a80b52891d41c4df36
wormholeracing <andyjm9@gmail.com>
2016-10-13 12:43:21 -0400

    Updated mapping in LoginController

--- src/main/java/org/ssa/ironyard/controller/LoginController.java
+++ src/main/java/org/ssa/ironyard/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -50,5 +51,11 @@ public class LoginController {
     @RequestMapping(value="", method=RequestMethod.GET)
     public View showLoginPage(){
 	return new InternalResourceView("/login.html");
+    }
+    
+    @RequestMapping(value = "/user/{userId}")
+    public String home(@PathVariable Integer userId)
+    {
+        return "/index.html";
     }
 }
