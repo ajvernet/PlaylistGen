2177e15f538b2ad2f144f07a72425738716c5a66
thurston ssa <thurston.nabe@theironyard.com>
2016-10-24 09:35:18 -0400

    add asynchronous support for finding latest shows

---
+++ src/test/java/org/ssa/ironyard/service/sandbox/AsyncAudioSearchTests.java
@@ -0,0 +1,78 @@
+package org.ssa.ironyard.service.sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.ssa.ironyard.model.Episode;
+import org.ssa.ironyard.model.Show;
+import org.ssa.ironyard.service.AudiosearchAuthorizationService;
+/**
+ *
+ * @author thurston
+ */
+public class AsyncAudioSearchTests 
+{
+    AsyncAudioService service;
+
+    @Before
+    public void setup()
+    {
+        this.service = new AsyncAudioService(new AudiosearchAuthorizationService());
+    }
+    
+    //@Test
+    public void config() throws Exception
+    {
+        //this.service.testShows(Arrays.asList(new Show(2428, "", "thumb")));
+    }
+    
+    
+    @Test
+    public void multiple() throws Exception
+    {
+        List<Episode> latestEpisodes = new ArrayList<>(18);
+        latestEpisodes.add(Episode.builder().episodeId(18053).show(new Show(14, "Fresh Air", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(5049).show(new Show(51, "Bookworm", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(112745).show(new Show(72, "NewsHour", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(3348).show(new Show(81, "The Nerdist", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(99786).show(new Show(311, "Storycorps", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(16605).show(new Show(353, "Freakonomics Radio", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(6329).show(new Show(358, "Stuff You Should Know", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(32181).show(new Show(368, "Ask Me Another", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(17113).show(new Show(381, "All Songs Considered", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(64151).show(new Show(399, "Stuff To Blow Your Mind", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(5084).show(new Show(461, "Slate's Amicus with Dahlia Lithwick", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(6908).show(new Show(464, "Slate Money", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(8686).show(new Show(521, "Note To Self", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(41218).show(new Show(1203, "CLUBCast - EDM", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(43098).show(new Show(1388, "Afrojack - JACKED Radio (Official Podcast)", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(39622).show(new Show(1477, "Mixhunt.co", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(177260).show(new Show(6164, "Wikishuffle", "")).build());
+        latestEpisodes.add(Episode.builder().episodeId(197244).show(new Show(7806, "House of Borgeous", "")).build());
+        
+        
+        List<Show> uniqueShows = new ArrayList<>(18);
+//        uniqueShows.add(new Show(464, "Slate Money", ""));
+//        uniqueShows.add(new Show(521, "Note To Self", ""));
+//        uniqueShows.add(new Show(72, "NewsHour", ""));
+//        uniqueShows.add(new Show(461, "Slate's Amicus", ""));
+//        uniqueShows.add(new Show(368, "Ask Me Another", ""));
+//        uniqueShows.add(new Show(381, "All Songs Considered", ""));
+//        uniqueShows.add(new Show(81, "The Nerdist", ""));
+//        uniqueShows.add(new Show(399, "Stuff To Blow Your Mind", ""));
+//        uniqueShows.add(new Show(51, "Bookworm", ""));
+//        uniqueShows.add(new Show(14, "Fresh Air", ""));
+//        uniqueShows.add(new Show(353, "Freakonomics Radio", ""));
+//        uniqueShows.add(new Show(358, "Stuff You Should Know", ""));
+//        uniqueShows.add(new Show(311, "Storycorps", ""));
+//        uniqueShows.add(new Show(1477, "Mixhunt.co", ""));
+//        uniqueShows.add(new Show(1388, "Afrojack", ""));
+//        uniqueShows.add(new Show(6164, "Wikishuffle", ""));
+//        uniqueShows.add(new Show(1203, "CLUBCast EDM", ""));
+//        uniqueShows.add(new Show(7806, "House of Bourgeous", ""));
+        
+        this.service.testShows(latestEpisodes);
+        
+    }
+}
---
+++ src/test/java/org/ssa/ironyard/service/sandbox/AsyncAudioService.java
@@ -0,0 +1,165 @@
+
+package org.ssa.ironyard.service.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.ssa.ironyard.model.AuthToken;
+import org.ssa.ironyard.model.Episode;
+import org.ssa.ironyard.model.Show;
+import org.ssa.ironyard.service.AudiosearchAuthorizationService;
+import org.ssa.ironyard.service.AudiosearchService;
+import org.ssa.ironyard.service.AudiosearchServiceImpl;
+import org.ssa.ironyard.service.mapper.ShowResult;
+
+/**
+ *
+ * @author thurston
+ */
+public class AsyncAudioService implements AudiosearchService
+{
+    static final Logger LOGGER = LogManager.getLogger(AudiosearchService.class);
+    
+    final AudiosearchAuthorizationService authorizer;
+    final AuthToken token;
+    final AsyncRestTemplate asyncTemplate;
+    public AsyncAudioService(AudiosearchAuthorizationService authorizer)
+    {
+        this.authorizer = authorizer;
+        this.token = authorizer.getAccessToken();
+        this.asyncTemplate = new AsyncRestTemplate();
+    }
+
+    
+    @Override
+    public List<Episode> searchEpisodesAlt(String genre, String searchText, Integer size)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Episode> getTasties()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getGenres()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    /**
+     * This is the one I want to implement
+     */
+    public List<Episode> getNewShowsByUserId(Integer userId)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    HttpEntity<String> getHeaders() {
+	HttpHeaders headers = new HttpHeaders();
+	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	headers.setContentType(MediaType.APPLICATION_JSON);
+	headers.set("Authorization", "Bearer " + this.token.getAccess_token());
+	HttpEntity<String> entity = new HttpEntity<>(headers);
+	//LOGGER.debug(headers.toString());
+	return entity;
+    }
+    
+    
+    void testShows(List<Episode> shows) throws Exception
+    {
+        LOGGER.info("Starting #testShows");
+        final String endPoint = AudiosearchServiceImpl.apiBaseUri + "/shows/";
+        final AsyncRestTemplate asyncRestTemplate = this.asyncTemplate;
+        
+        
+        final Queue<ShowResult> results = new ConcurrentLinkedQueue<>();
+        final List<ListenableFuture<ResponseEntity<ShowResult>>> futures = new ArrayList<>();
+        
+        final HttpEntity<String> headers = getHeaders();
+        for (int index = 1; index < shows.size(); index++)
+        {
+            final Episode episode = shows.get(index);
+            ListenableFuture<ResponseEntity<ShowResult>> futureResponse = asyncRestTemplate.exchange(endPoint + episode.getShow().getId().toString(), 
+                                                                                                     HttpMethod.GET, 
+                                                                                                     headers,
+                                                                                                     ShowResult.class);
+            futures.add(futureResponse);
+            futureResponse.addCallback(new ListenableFutureCallback<ResponseEntity<ShowResult>>() 
+                {
+                    @Override
+                    public void onSuccess(ResponseEntity<ShowResult> response)
+                    {
+                        ShowResult sr = response.getBody();
+                        sr.setEpisode(episode);
+                        sr.filter();
+                        results.add(sr);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable thrwbl)
+                    {
+                        LOGGER.warn("show request {} failed -> {}", episode.getShow(), thrwbl);
+                    }
+                });
+        }
+        /*
+        *  Always have the join thread do some work as well, use up it's CPU time slice/quantum
+        *  
+        */
+        final Episode episode = shows.get(0);
+        try
+        {
+            ResponseEntity<ShowResult> response = asyncRestTemplate.getRestOperations().exchange(endPoint + episode.getShow().getId().toString(), 
+                                                                                                 HttpMethod.GET,
+                                                                                                 headers,
+                                                                                                 ShowResult.class);
+            ShowResult body = response.getBody();
+            body.setEpisode(episode);
+            body.filter();
+            results.add(body);
+        }
+        catch (Exception ex)
+        {
+            LOGGER.info(ex);
+        }
+        for (ListenableFuture<?> future : futures)
+        {
+            try
+            {
+                future.get(1_000L, TimeUnit.MILLISECONDS); // we naiively block, but we're a joiner
+            }
+            catch (TimeoutException timeout)
+            {
+                LOGGER.info(timeout);
+                future.cancel(true);
+            }
+            catch (Exception ignore)
+            {
+                
+            }
+        }
+        
+        LOGGER.info("{}", results);
+        LOGGER.info("episodes -> {}", results.stream().flatMap(sr -> sr.getEpisode_ids().stream()).distinct().count());
+        
+    }
+}
